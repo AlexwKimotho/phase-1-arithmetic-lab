9a560cab87d948e005ac48e218e4332c531cd6bc
Alex-Wachira-Kimotho <alex.wachira@student.moringaschool.com>
2022-08-16 07:56:38 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
 // Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 22;
+let num4 = 6;
+let mod = num3 % num4;
+let max = Math.max = (5, 10, 15, 20);
